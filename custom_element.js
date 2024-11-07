0d5c20399fbfd3af35d377a12e3ffb3d70a7c581
emmashackell <emmashacks9@gmail.com>
2024-11-07 01:45:58 +0000

    final

---
+++ custom_element.js
@@ -0,0 +1,17 @@
+alert("Emma was here!");
+
+class Centre extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div style="text-align:center">${this.innerHTML}</div>`
+    }
+}
+
+customElements.define("x-center", Centre);
+
+class color extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div style="color:red">${this.innerHTML}</div>`
+    }
+}
+
+customElements.define("x-red", color);
